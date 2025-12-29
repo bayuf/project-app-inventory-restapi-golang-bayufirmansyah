9bc7247320d0ed745a0f9b2231b57ef418cf8f34
Bayu Firmansyah <bayufirmansyah203@gmail.com>
2025-12-29 12:42:52 +0700

    feat: add config for system

---
+++ utils/config.go
@@ -0,0 +1,48 @@
+package utils
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Configuration struct {
+	JWTKey      string
+	AppName     string
+	Port        string
+	Debug       bool
+	PageLimit   int
+	PathLogging string
+	DB          DatabaseConfiguration
+}
+
+type DatabaseConfiguration struct {
+	DBName   string
+	UserName string
+	Password string
+	HostName string
+	SSLMode  string
+}
+
+func ReadConfiguration() (Configuration, error) {
+	viper.SetConfigFile(".env")
+	viper.SetConfigType("env")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return Configuration{}, err
+	}
+
+	return Configuration{
+		JWTKey:      viper.GetString("JWT_KEY"),
+		AppName:     viper.GetString("APP_NAME"),
+		Port:        viper.GetString("PORT"),
+		Debug:       viper.GetBool("DEBUG"),
+		PageLimit:   viper.GetInt("PAGE_LIMIT"),
+		PathLogging: viper.GetString("PATH_LOGGING"),
+		DB: DatabaseConfiguration{
+			DBName:   viper.GetString("DB_NAME"),
+			UserName: viper.GetString("DB_USERNAME"),
+			Password: viper.GetString("DB_PASSWORD"),
+			HostName: viper.GetString("DB_HOSTNAME"),
+			SSLMode:  viper.GetString("DB_SSL_MODE"),
+		},
+	}, nil
+}
