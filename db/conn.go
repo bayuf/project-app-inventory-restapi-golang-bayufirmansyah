caa0b7e559735970ff9aa2f0018ba6215a9e7a0d
Bayu Firmansyah <bayufirmansyah203@gmail.com>
2025-12-29 12:42:18 +0700

    fix: db connection with config file

--- db/conn.go
+++ db/conn.go
@@ -2,10 +2,10 @@ package db
 
 import (
 	"context"
-	"errors"
-	"os"
+	"fmt"
 	"time"
 
+	"github.com/bayuf/project-app-inventory-restapi-golang-bayufirmansyah/utils"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -18,35 +18,31 @@ type PgxIface interface {
 	Exec(ctx context.Context, sql string, args ...any) (pgconn.CommandTag, error)
 }
 
-func Connect(log *zap.Logger) (*pgxpool.Pool, error) {
-
-	dbUrl := os.Getenv("DB_URL")
-	if dbUrl == "" {
-		log.Error("db URL is empty")
-		return nil, errors.New("db URL is empty")
-	}
-
-	config, err := pgxpool.ParseConfig(dbUrl)
+func Connect(log *zap.Logger, config *utils.DatabaseConfiguration) (*pgxpool.Pool, error) {
+	// "user password dbname sslmode host"
+	dbUrl := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s host=%s",
+		config.UserName, config.Password, config.DBName, config.SSLMode, config.HostName)
+	poolConfig, err := pgxpool.ParseConfig(dbUrl)
 	if err != nil {
 		log.Error("cant parse db config", zap.Error(err))
 		return nil, err
 	}
 
 	// db pool config
-	config.MaxConns = 10
-	config.MinConns = 2
-	config.MaxConnLifetime = time.Hour
-	config.MaxConnIdleTime = 30 * time.Minute
-	config.HealthCheckPeriod = time.Minute
+	poolConfig.MaxConns = 10
+	poolConfig.MinConns = 2
+	poolConfig.MaxConnLifetime = time.Hour
+	poolConfig.MaxConnIdleTime = 30 * time.Minute
+	poolConfig.HealthCheckPeriod = time.Minute
 
-	dbPool, err := pgxpool.NewWithConfig(context.Background(), config)
+	dbPool, err := pgxpool.NewWithConfig(context.Background(), poolConfig)
 	if err != nil {
-		log.Error("cant create db pool")
+		log.Error("cant create db pool", zap.Error(err))
 		return nil, err
 	}
 
 	if err := dbPool.Ping(context.Background()); err != nil {
-		log.Error("cant ping to db")
+		log.Error("cant ping to db", zap.Error(err))
 		return nil, err
 	}
 
--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"log"
+
+	"github.com/bayuf/project-app-inventory-restapi-golang-bayufirmansyah/db"
+	"github.com/bayuf/project-app-inventory-restapi-golang-bayufirmansyah/utils"
+)
+
 func main() {
 
+	// init config
+	config, err := utils.ReadConfiguration()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// init Logger
+	logger, err := utils.InitLogger(config.PathLogging, config.Debug)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// init DB
+	dBConn, err := db.Connect(logger, &config.DB)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = dBConn
 }
