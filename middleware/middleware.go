847e45ee0dfb857301cd1f6dcc6b90dde33e1e1b
Bayu Firmansyah <bayufirmansyah203@gmail.com>
2025-12-30 13:22:14 +0700

    feat: initialize middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,1 +1,18 @@
 package middleware
+
+import (
+	"github.com/bayuf/project-app-inventory-restapi-golang-bayufirmansyah/service"
+	"go.uber.org/zap"
+)
+
+type Middleware struct {
+	Service *service.Service
+	Logger  *zap.Logger
+}
+
+func NewCustomMiddleware(service *service.Service, log *zap.Logger) *Middleware {
+	return &Middleware{
+		Service: service,
+		Logger:  log,
+	}
+}
--- router/router.go
+++ router/router.go
@@ -2,13 +2,30 @@ package router
 
 import (
 	"github.com/bayuf/project-app-inventory-restapi-golang-bayufirmansyah/handler"
+	middlewareCustom "github.com/bayuf/project-app-inventory-restapi-golang-bayufirmansyah/middleware"
 	"github.com/bayuf/project-app-inventory-restapi-golang-bayufirmansyah/service"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"go.uber.org/zap"
 )
 
 func NewRouter(handler *handler.Handler, service *service.Service, log *zap.Logger) *chi.Mux {
 	r := chi.NewRouter()
+	mw := middlewareCustom.NewCustomMiddleware(service, log)
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Mount("api", Api(handler, mw))
+
+	return r
+}
+
+func Api(handler *handler.Handler, mw *middlewareCustom.Middleware) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Route("/", func(r chi.Router) {
+	})
 
 	return r
 }
